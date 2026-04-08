18f30fd6d593b79e350a66a8bfda911974117fc2
Dev-Ibrahim2019 <ialashqar.dev64@gmail.com>
2026-04-08 14:40:08 +0300

    chort: API connection && Authentication

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\ApiController;
+use App\Http\Requests\V1\LoginUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends ApiController
+{
+    public function login(LoginUserRequest $request)
+    {
+        $request->validated($request->all());
+
+        if (! Auth::attempt($request->only('email', 'password'))) {
+            return $this->error('Invalid Credentials', 401);
+        }
+
+        $user = User::firstWhere('email', $request->email);
+
+        return $this->ok(
+            'Authenticated',
+            [
+                'token' => $user->createToken('Api token for ' . $user->email)->plainTextToken,
+            ]   
+        );
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->ok('User loged out ');
+    }
+
+    public function register()
+    {
+        // return $this->ok('register');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthController;
+use App\Models\User;
+// use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return User::whereId(2)->get(['name', 'email']);
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth:sanctum')->get("/auth/me", function (Request $request) {
+    return response()->json(['user' => $request->user()]);
+});
